60de54fec08ba966e75f5193f34d86574412592b
Lakshay <garglakshay2002@gmail.com>
2024-08-21 22:33:44 +0530

    complete project

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,4 +7,26 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'todoApp';
+
+  tasks=[
+    { id: 1, task: 'Bring bread', status: 'Done', isEditing: false },
+    { id: 2, task: 'Go for a walk', status: 'Done', isEditing: false },
+    { id: 3, task: 'Complete this', status: 'Pending', isEditing: false },
+    { id: 4, task: 'Play football', status: 'Pending', isEditing: false },
+    { id: 5, task: 'Call Mom', status: 'Pending', isEditing: false }
+  ]
+
+
+  toggleStatus(task: any) {
+    task.status = task.status === 'Done' ? 'Pending' : 'Done';
+  }
+
+  deleteTask(task: any) {
+    this.tasks = this.tasks.filter(t => t !== task);
+  }
+
+  
+  toggleEdit(task: any) {
+    task.isEditing = !task.isEditing;
+  }
 }
